b1a462fffce50e349a2ef850d060c4600324e139
Rentea Claudiu <renteaclaudiu@yahoo.com>
2017-12-06 20:13:17 +0200

    Update portScanner.java

--- JPortScanner/src/JPortScan/portScanner.java
+++ JPortScanner/src/JPortScan/portScanner.java
@@ -6,7 +6,7 @@ public class portScanner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		scanner scanPort=new scanner();
-		System.out.println("Insert ip or adress: ");
+		System.out.println("Insert ip or address: ");
 		Scanner input=new Scanner(System.in);
 		String adressIp=input.next();
 		scanPort.ipFinder(adressIp);
